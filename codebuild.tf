f6e8f1efc0e8b1c615aea4b756b2c9d5ef30fabf
Chris McKinnel <chrismckinnel@gmail.com>
2020-05-13 20:37:49 +1200

    Upgrade tf version

--- codebuild.tf
+++ codebuild.tf
@@ -46,7 +46,7 @@ resource "aws_codebuild_project" "example" {
   name          = "test"
   description   = "test_codebuild_project"
   build_timeout = "5"
-  service_role  = "${aws_iam_role.codebuild_role.arn}"
+  service_role  = aws_iam_role.codebuild_role.arn
 
   artifacts {
     type = "CODEPIPELINE"
@@ -54,7 +54,7 @@ resource "aws_codebuild_project" "example" {
 
   cache {
     type     = "S3"
-    location = "${aws_s3_bucket.codebuild_bucket.bucket}"
+    location = aws_s3_bucket.codebuild_bucket.bucket
   }
 
   environment {
