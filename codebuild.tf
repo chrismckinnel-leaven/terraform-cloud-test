e44e8739bde19c0020a9672ad2f355f2bef5c94c
Chris McKinnel <chrismckinnel@gmail.com>
2020-05-13 20:41:40 +1200

    Add CP as artifact

--- codebuild.tf
+++ codebuild.tf
@@ -49,7 +49,7 @@ resource "aws_codebuild_project" "example" {
   service_role  = aws_iam_role.codebuild_role.arn
 
   artifacts {
-    type = "NO_ARTIFACTS"
+    type = "CODEPIPELINE"
   }
 
   cache {
