c0ead50aa6f57ef18d21ed2260b2a3dc429359ca
Chris McKinnel <chrismckinnel@gmail.com>
2020-05-13 16:13:31 +1200

    Add basic codepipeline and provider

---
+++ main.tf
@@ -0,0 +1,89 @@
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket = "test-bucket"
+  acl    = "private"
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "test-role"
+
+  assume_role_policy = "${data.aws_iam_policy_document.codepipeline-assume-role-policy.json}"
+}
+
+data "aws_iam_policy_document" "codepipeline-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "codepipeline-role-policy" {
+  statement {
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.codepipeline_bucket.arn}",
+      "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "codepipeline_policy"
+  role = "${aws_iam_role.codepipeline_role.id}"
+
+  policy = "${data.aws_iam_policy_document.codepipeline-role-policy.json}"
+}
+
+data "aws_kms_alias" "s3kmskey" {
+  name = "alias/myKmsKey"
+}
+
+resource "aws_codepipeline" "codepipeline" {
+  name     = "tf-test-pipeline"
+  role_arn = "${aws_iam_role.codepipeline_role.arn}"
+
+  artifact_store {
+    location = "${aws_s3_bucket.codepipeline_bucket.bucket}"
+    type     = "S3"
+
+    encryption_key {
+      id   = "${data.aws_kms_alias.s3kmskey.arn}"
+      type = "KMS"
+    }
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner  = "chrismckinnel-leaven"
+        Repo   = "terraform-cloud-test"
+        Branch = "master"
+      }
+    }
+  }
+}
