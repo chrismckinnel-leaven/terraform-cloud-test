0819c6d64b9a42eae9336b0dd020683d026eea2f
Chris McKinnel <chrismckinnel@gmail.com>
2020-05-13 20:40:07 +1200

    Add artifacts

--- codebuild.tf
+++ codebuild.tf
@@ -48,6 +48,10 @@ resource "aws_codebuild_project" "example" {
   build_timeout = "5"
   service_role  = aws_iam_role.codebuild_role.arn
 
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
   cache {
     type     = "S3"
     location = aws_s3_bucket.codebuild_bucket.bucket
