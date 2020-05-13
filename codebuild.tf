dd7a090eb5f1393e70c6d4cefbac4047e9e378fb
Chris McKinnel <chrismckinnel@gmail.com>
2020-05-13 20:38:59 +1200

    Add CP as a source

--- codebuild.tf
+++ codebuild.tf
@@ -48,10 +48,6 @@ resource "aws_codebuild_project" "example" {
   build_timeout = "5"
   service_role  = aws_iam_role.codebuild_role.arn
 
-  artifacts {
-    type = "CODEPIPELINE"
-  }
-
   cache {
     type     = "S3"
     location = aws_s3_bucket.codebuild_bucket.bucket
@@ -77,6 +73,6 @@ resource "aws_codebuild_project" "example" {
   }
 
   source {
-    type = "NO_SOURCE"
+    type = "CODEPIPELINE"
   }
 }
